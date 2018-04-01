db18a043d0eb9630571423c25c8af7aeb84315fe
igor.onischuk <spazmolitik@gmail.com>
2018-04-01 22:11:35 +0300

    Homework 10

--- js/init.js
+++ js/init.js
@@ -1,13 +1,19 @@
-$( function() {
+$(function () {
     // Init tabs
-    $( "#tabs" ).tabs();
-    $( "#tabs-portfolio" ).tabs();
+    $("#tabs").tabs();
+    $("#tabs-portfolio").tabs();
 
     // init slick carousel //
     $('.reviews-carousel').slick({
         dots: true,
         prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class ="fa fa-chevron-left"</i></div></div>',
         nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class ="fa fa-chevron-right"</i></div></div>',
+    });
+
+    // Init slick-nav //
+    $('.header-nav').slicknav({
+        appendTo: '.header .container-fluid',
+        label: ''
     });
 
 });
